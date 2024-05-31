e16c4ae80317e89be941bd03381f9affb1b17a55
jaksondeoliveiramartins <jaomfive@hotmail.com>
2024-05-31 18:25:07 -0300

    adicionado arquivos e atualiazando o Readme

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp( {
-    apiKey: "AIzaSyBbcCo7QwPBiFqZ_ZKkoG2oO5ooKKYpuCo",
-    authDomain: "instagram-clone-fdfea.firebaseapp.com",
-    projectId: "instagram-clone-fdfea",
-    storageBucket: "instagram-clone-fdfea.appspot.com",
-    messagingSenderId: "435129863409",
-    appId: "1:435129863409:web:2be02eaca54f048bfc2d50",
-    measurementId: "G-S6H394GN0C"
+  apiKey: "AIzaSyBbcCo7QwPBiFqZ_ZKkoG2oO5ooKKYpuCo",
+  authDomain: "instagram-clone-fdfea.firebaseapp.com",
+  projectId: "instagram-clone-fdfea",
+  storageBucket: "instagram-clone-fdfea.appspot.com",
+  messagingSenderId: "435129863409",
+  appId: "1:435129863409:web:b832302145ee4badfc2d50",
+  measurementId: "G-0FPNPQND4P"
   });
 
 const db = firebase.firestore();
