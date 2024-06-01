ef3157c246e49a01589008fe1760e0d3c1db37c8
jaksondeoliveiramartins <jaomfive@hotmail.com>
2024-05-31 21:31:32 -0300

    add novo auth firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp( {
-  apiKey: "AIzaSyB0PPXFbDY01Vf3nRA63hDn22D0ws36P2Y",
-  authDomain: "woven-sensor-416923.firebaseapp.com",
-  projectId: "woven-sensor-416923",
-  storageBucket: "woven-sensor-416923.appspot.com",
-  messagingSenderId: "833838519780",
-  appId: "1:833838519780:web:993b1058fdd48c61df63f1",
-  measurementId: "G-NVFV2WPBSN"
+  apiKey: "AIzaSyAO7fySZLhsntfISdLnh9XE5zj40P4QM-I",
+  authDomain: "insta-app-a00c0.firebaseapp.com",
+  projectId: "insta-app-a00c0",
+  storageBucket: "insta-app-a00c0.appspot.com",
+  messagingSenderId: "563190886935",
+  appId: "1:563190886935:web:a72ba18954f5ba92bbe252",
+  measurementId: "G-R2VPV86CS9"
  
   });
 
